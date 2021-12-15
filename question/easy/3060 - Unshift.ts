74ee0f0ed3b4fb474479af10cb1c95856916ad5f
SilveryStar <1104247794@qq.com>
2021-12-15 11:58:11 +0800

    warm-up and easy questions

---
+++ question/easy/3060 - Unshift.ts
@@ -0,0 +1,32 @@
+/*
+  3060 - Unshift
+  -------
+  by jiangshan (@jiangshanmeta) #简单 #array
+
+  ### 题目
+
+  实现类型版本的 ```Array.unshift```。
+
+  举例，
+
+  ```typescript
+  type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/3060/zh-CN
+*/
+
+
+/* _____________ 你的代码 _____________ */
+
+type Unshift<T extends any[], U> = [ U, ...T ];
+
+
+/* _____________ 测试用例 _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<Unshift<[], 1>, [1]>>,
+    Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2,]>>,
+    Expect<Equal<Unshift<['1', 2, '3'],boolean>, [boolean, '1', 2, '3']>>,
+]
